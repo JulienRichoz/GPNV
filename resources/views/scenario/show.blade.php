e5769cc015e90847cd2919a78353db9e431af0b9
Fabio Marques <fabio-manuel.silva-marques@cpnv.ch>
2017-03-24 11:56:37 +0100

    added function show scenario from objectifs

--- resources/views/scenario/show.blade.php
+++ resources/views/scenario/show.blade.php
@@ -3,14 +3,14 @@
   <p>Se scénario permet de vérifier si l'application fonctionne correctement</p>
 <form>
   <div class="elements">
-    <h2>Elements</h2>
+    <h2>Etapes</h2>
     <table class="table table-hover">
       <thead>
         <tr>
           <th>#</th>
           <th>Action</th>
           <th>Réponse</th>
-          <th>Résultat</th>
+          <!--<th>Résultat</th>-->
         </tr>
       </thead>
       <tbody>
@@ -18,45 +18,87 @@
           <td>1</td>
           <td>Cliquer sur le bouton "Créer un nouveau project"</td>
           <td>la page nouveau project s'affiche</td>
-          <td>
+          <!--<td>
             <textarea></textarea>
             <i class="btn btn-success fa fa-check validate"></i>
             <i class="btn btn-danger fa fa-times reject"></i>
             <input type="hidden" value="" name="state">
-          </td>
+          </td>-->
         </tr>
         <tr data-imgurl="{{ URL::asset('images/scenario2.png') }}">
           <td>2</td>
           <td>Cliquer sur le bouton "Créer un nouveau project"</td>
           <td>la page nouveau project s'affiche</td>
-          <td>
-            <textarea></textarea>
-            <i class="btn btn-success fa fa-check validate"></i>
-            <i class="btn btn-danger fa fa-times reject"></i>
-            <input type="hidden" value="" name="state">
-          </td>
         </tr>
         <tr data-imgurl="{{ URL::asset('images/scenario3.png') }}">
           <td>3</td>
           <td>Cliquer sur le bouton "Créer un nouveau project"</td>
           <td>la page nouveau project s'affiche</td>
-          <td>
-            <textarea></textarea>
-            <i class="btn btn-success fa fa-check validate"></i>
-            <i class="btn btn-danger fa fa-times reject"></i>
-            <input type="hidden" value="" name="state">
-          </td>
         </tr>
         <tr data-imgurl="{{ URL::asset('images/scenario4.png') }}">
           <td>4</td>
           <td>Cliquer sur le bouton "Créer un nouveau project"</td>
           <td>la page nouveau project s'affiche</td>
-          <td>
-            <textarea></textarea>
-            <i class="btn btn-success fa fa-check validate"></i>
-            <i class="btn btn-danger fa fa-times reject"></i>
-            <input type="hidden" value="" name="state">
-          </td>
+        </tr>
+        <tr data-imgurl="{{ URL::asset('images/scenario1.png') }}">
+          <td>5</td>
+          <td>Cliquer sur le bouton "Créer un nouveau project"</td>
+          <td>la page nouveau project s'affiche</td>
+        </tr>
+        <tr data-imgurl="{{ URL::asset('images/scenario2.png') }}">
+          <td>6</td>
+          <td>Cliquer sur le bouton "Créer un nouveau project"</td>
+          <td>la page nouveau project s'affiche</td>
+        </tr>
+        <tr data-imgurl="{{ URL::asset('images/scenario3.png') }}">
+          <td>7</td>
+          <td>Cliquer sur le bouton "Créer un nouveau project"</td>
+          <td>la page nouveau project s'affiche</td>
+        </tr>
+        <tr data-imgurl="{{ URL::asset('images/scenario4.png') }}">
+          <td>8</td>
+          <td>Cliquer sur le bouton "Créer un nouveau project"</td>
+          <td>la page nouveau project s'affiche</td>
+        </tr>
+        <tr data-imgurl="{{ URL::asset('images/scenario1.png') }}">
+          <td>9</td>
+          <td>Cliquer sur le bouton "Créer un nouveau project"</td>
+          <td>la page nouveau project s'affiche</td>
+        </tr>
+        <tr data-imgurl="{{ URL::asset('images/scenario2.png') }}">
+          <td>10</td>
+          <td>Cliquer sur le bouton "Créer un nouveau project"</td>
+          <td>la page nouveau project s'affiche</td>
+        </tr>
+        <tr data-imgurl="{{ URL::asset('images/scenario3.png') }}">
+          <td>11</td>
+          <td>Cliquer sur le bouton "Créer un nouveau project"</td>
+          <td>la page nouveau project s'affiche</td>
+        </tr>
+        <tr data-imgurl="{{ URL::asset('images/scenario4.png') }}">
+          <td>12</td>
+          <td>Cliquer sur le bouton "Créer un nouveau project"</td>
+          <td>la page nouveau project s'affiche</td>
+        </tr>
+        <tr data-imgurl="{{ URL::asset('images/scenario1.png') }}">
+          <td>13</td>
+          <td>Cliquer sur le bouton "Créer un nouveau project"</td>
+          <td>la page nouveau project s'affiche</td>
+        </tr>
+        <tr data-imgurl="{{ URL::asset('images/scenario2.png') }}">
+          <td>14</td>
+          <td>Cliquer sur le bouton "Créer un nouveau project"</td>
+          <td>la page nouveau project s'affiche</td>
+        </tr>
+        <tr data-imgurl="{{ URL::asset('images/scenario3.png') }}">
+          <td>15</td>
+          <td>Cliquer sur le bouton "Créer un nouveau project"</td>
+          <td>la page nouveau project s'affiche</td>
+        </tr>
+        <tr data-imgurl="{{ URL::asset('images/scenario4.png') }}">
+          <td>16</td>
+          <td>Cliquer sur le bouton "Créer un nouveau project"</td>
+          <td>la page nouveau project s'affiche</td>
         </tr>
       </tbody>
     </table>
