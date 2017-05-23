3f7adc2eae1fa24e99a109378c4f32ff3434d838
raph-u <raphael.bazzari@cpnv.ch>
2017-05-23 16:38:56 +0200

    Updated button label for users addition
    The button used to add users in the project now simply contains a label reading "ajouter"

--- resources/views/project/addUsers.blade.php
+++ resources/views/project/addUsers.blade.php
@@ -32,7 +32,7 @@
      <div class="form-group">
          <div class="col-md-6 col-md-offset-4">
              <button type="submit" class="btn btn-primary">
-                 <i class="fa fa-btn fa-plus"></i>Ajouter un/des utilisateur(s)
+                 Ajouter
              </button>
          </div>
      </div>
